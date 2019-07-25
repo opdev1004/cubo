458af55ae3b1f37be838f068ac6c0c25e14e8536
Nicholas Rowley <nick-1.03@hotmail.com>
2019-07-25 18:05:56 +1200

    Added initial code for player movement and jumping (incomplete)

---
+++ Cubo/Assets/Scripts/Player/PlayerControls.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    PlayerMovement playerMovement;
+    
+    //controls
+    private List<KeyCode> p1JumpKey = new List<KeyCode> { KeyCode.Space, KeyCode.Joystick1Button0 };
+    private List<KeyCode> p2JumpKey = new List<KeyCode> { KeyCode.RightControl, KeyCode.Joystick2Button0 };
+    private List<KeyCode> jumpKey;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        if (this.gameObject.name.Equals("Player1"))
+        {
+            jumpKey = p1JumpKey;
+        } else
+            if (this.gameObject.name.Equals("Player2"))
+        {
+            jumpKey = p2JumpKey;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!playerMovement.isJumping)
+        {
+            foreach (KeyCode key in jumpKey)
+            {
+                if (Input.GetKeyDown(key))
+                {
+                    playerMovement.JumpStart();
+                }
+            }
+        }
+    }
+}
---
+++ Cubo/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //objects
+    Rigidbody m_Rigidbody;
+    BoxCollider m_BoxCollider;
+
+    public float turnSpeed = 20f; //turn speed in radians per second
+    public float movementSpeed = 4.0f;
+
+    //jumping
+    public float jumpForce = 10.0f; //jump force per second
+    public float jumpDuration = 0.5f; //jump duration in seconds
+
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+    
+    float jumpTime;
+    public bool isJumping { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_BoxCollider = GetComponent<BoxCollider>();
+        jumpTime = 0f;
+    }
+
+    /**
+	* Instead of being called before every rendered frame like Update(),
+	* FixedUpdate is called before the physics system solves any collisions and other interactions that have happened.
+	* By default it is called exactly 50 times every second.
+	*/
+    void FixedUpdate()
+    {
+        //Check for movement
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+        if (isWalking)
+        {
+            Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+            m_Rotation = Quaternion.LookRotation(desiredForward);
+
+            m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * movementSpeed * Time.deltaTime);
+
+            m_Rigidbody.MoveRotation(m_Rotation);
+        }
+
+        Jump();
+    }
+
+    //runs every frame
+    void Update()
+    {
+        //
+    }
+
+    //causes the player to start jumping. use only after checking for player jumps as box cast is resource intensive.
+    public void JumpStart()
+    {
+         //check if the player is standing on a solid object (for cube shaped objects)
+         if (Physics.BoxCast(m_Rigidbody.position, new Vector3(m_BoxCollider.size.x / 2.05f, m_BoxCollider.size.y / 2.05f, m_BoxCollider.size.z / 2.05f), Vector3.down, Quaternion.LookRotation(Vector3.down), m_BoxCollider.size.y * 0.05f))
+         {
+             isJumping = true;
+             jumpTime = 0.5f;
+         }
+    }
+    
+    //Moves the character up while it is jumping.
+    void Jump()
+    {
+        if (isJumping)
+        {
+            if (jumpTime > 0)
+            {
+                Vector3 jumpvector = new Vector3(0, jumpForce, 0);
+                m_Rigidbody.MovePosition(m_Rigidbody.position + jumpvector * movementSpeed * Time.deltaTime);
+                jumpTime -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+    }
+}
